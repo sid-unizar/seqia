066c857d9bd32ea44e3ec6af246fa811790b1756
miguelopezotal <74180331+miguelopezotal@users.noreply.github.com>
2024-09-16 14:15:51 +0200

    Update download_geonames.sh

--- download_geonames.sh
+++ download_geonames.sh
@@ -1,7 +1,8 @@
 #Download and unzip Geonames Spain dumped local file
 
 mkdir ./seqia/geonames/
-wget ./seqia/geonames/ES.zip https://download.geonames.org/export/dump/ES.zip
+wget https://download.geonames.org/export/dump/ES.zip
+mv ./ES.zip ./seqia/geonames/
 mkdir ./seqia/geonames/ES/
 unzip ./seqia/geonames/ES.zip -d ./seqia/geonames/ES/
 rm ./seqia/geonames/ES.zip
