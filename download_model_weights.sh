16e0db3741275e51bbab666c456440cc92eff07a
miguelopezotal <74180331+miguelopezotal@users.noreply.github.com>
2025-02-06 10:06:48 +0100

    Update download_model_weights.sh

--- download_model_weights.sh
+++ download_model_weights.sh
@@ -1,7 +1,7 @@
 #Script that downloads all model weight's from the Github repository.
-#This comes from a PRIVATE repo, so you'll need to configure all settings to access this data from your machine. You will also need to install the GitHub CLI commands
+#You will need to install the GitHub CLI commands
 #and setup the login credentials accordingly.
-#If you can't use this script, simply download the files manually from the Github page and transfer them to your machine.
+#If you can't run this script, simply download the files manually from the Github page and transfer them to your machine.
 
 declare -a arr=("binary binary_model" "Agricultura impacts/Agricultura" "Ganaderia impacts/Ganadería" "Recursos.hidricos impacts/Recursos_hídricos" "Energetico impacts/Energético")
 
